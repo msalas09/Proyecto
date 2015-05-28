ea64f9b259c7da5413e52fd1cf05ed0b06a9b8b7
msalas09 <msalas09@gmail.com>
2015-05-27 20:30:59 -0600

    Creacion del proyecto

---
+++ PV_Web/PV_Web/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("PV_Web")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("PV_Web")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("aa983420-3756-4903-aa96-edddb4eee9e3")> 
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o aceptar los valores predeterminados de los números de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
